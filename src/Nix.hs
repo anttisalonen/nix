175268582887b19e99651d7f7317cfdc26846dd6
Antti Salonen <ajsalonen@gmail.com>
2010-04-03 15:44:52 +0200

    add add command

--- src/Nix.hs
+++ src/Nix.hs
@@ -2,7 +2,7 @@ module Main
 where
 
 import Data.List (intercalate)
-import Control.Monad (liftM)
+import Control.Monad (when, liftM)
 import System.Exit
 import System.Directory
 import System (getArgs, getProgName)
@@ -82,7 +82,7 @@ setGlobalName n = do
 readGlobalName :: IO String
 readGlobalName = 
   do fpath <- globalconfigfile
-     exists <- doesFileExist fpath
+     exists <- doesDirectoryExist fpath
 
      if exists
        then do
@@ -94,11 +94,11 @@ readGlobalName =
 
   where flipOut fpath = throwIO $ 
           mkIOError doesNotExistErrorType 
-             ("Could not read global configuration file - run nix init first.") 
+             ("Could not read global configuration file - run nix config first.") 
              Nothing (Just fpath)
 
 handleConfig args = do
-  opts <- doArgs configOptions defaultConfigOptions [(\o -> not (null (configName o)))] "config" args
+  (opts, _) <- doArgs configOptions defaultConfigOptions [(\o -> not (null (configName o)))] "config" args False
   putStrLn $ "Your name: " ++ (configName opts)
   setGlobalName (configName opts)
 
@@ -112,24 +112,67 @@ doArgs
   -> [a -> Bool]         -- validation functions on parsed options
   -> String              -- name of command for error message
   -> [String]            -- cmd line arguments
-  -> IO a                -- parsed data (function exits on failure)
-doArgs opts defopts validfuncs cmd args = do
+  -> Bool                -- whether nonopts are allowed
+  -> IO (a, [String])    -- parsed data and messages (function exits on failure)
+doArgs opts defopts validfuncs cmd args allownonopts = do
   let (actions, nonOpts, msgs) = getOpt Permute opts args
-  if null nonOpts && null msgs
+  if (null nonOpts || allownonopts) && null msgs
     then do
       let finalopts = foldl (flip ($)) defopts actions
       if and $ isvalid validfuncs finalopts
-        then return finalopts
-        else putStrLn (usageInfo ("nix " ++ cmd) opts) >> exitWith (ExitFailure 1)
-    else putStrLn (usageInfo ("nix " ++ cmd) opts) >> exitWith (ExitFailure 1)
+        then return (finalopts, nonOpts)
+        else cmdUsage cmd opts
+    else cmdUsage cmd opts
 
 nixdirname = ".nix"  
 
 handleInit _ = do
   createDirectoryIfMissing False nixdirname
-  putStrLn "Created directory .nix"
+  putStrLn $ "Created directory " ++ nixdirname
 
-handleAdd = error "add not supported yet"
+addOptions :: [(OptDescr (AddOptions -> AddOptions))]
+addOptions = [
+    Option ['m'] ["message"] (ReqArg setMessage "message") "message"
+  ]
+
+data AddOptions = AddOptions { title :: String
+                             , message :: String }
+  deriving (Show, Read)
+
+setTitle :: String -> AddOptions -> AddOptions
+setTitle s c = c{title = s}
+
+setMessage :: String -> AddOptions -> AddOptions
+setMessage s c = c{message = s}
+
+defaultAddOptions = AddOptions "" ""
+
+-- exits.
+cmdUsage cmd opts = putStrLn (usageInfo ("nix " ++ cmd) opts) >> exitWith (ExitFailure 1)
+
+-- throws error if not init.
+checkIsInit = do
+  exists <- doesDirectoryExist nixdirname
+  when (not exists) $ throwIO $ 
+    mkIOError doesNotExistErrorType 
+       ("Could not find nix directory - run nix init first.") 
+       Nothing (Just nixdirname)
+
+handleAdd args = do
+  (opts, nonopts) <- doArgs addOptions defaultAddOptions [] "add" args True
+  if length nonopts /= 1
+    then putStrLn "You must tell me the title of the issue." >> cmdUsage "add" addOptions
+    else do
+      putStrLn $ "Adding " ++ (head nonopts) ++ " - " ++ message opts
+      add (setTitle (head nonopts) opts)
+
+issueFilePath :: String -> String
+issueFilePath t = nixdirname ++ "/" ++ t
+
+add :: AddOptions -> IO ()
+add opt = do
+  checkIsInit
+  writeFile (issueFilePath (title opt)) (show opt)
 
 handleDep = error "dep not supported yet"
 handleComment = error "comment not supported yet"
