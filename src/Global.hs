a0a8cadaf37190d98becf412586ccfd6d5ea816d
Antti Salonen <ajsalonen@gmail.com>
2010-04-04 05:19:13 +0200

    fix reading global name

--- src/Global.hs
+++ src/Global.hs
@@ -3,7 +3,7 @@ where
 
 import System.Directory
 import Control.Exception (throwIO)
-import System.IO.Error (mkIOError, doesNotExistErrorType)
+import System.IO.Error (mkIOError, userErrorType, doesNotExistErrorType)
 import Prelude hiding (catch)
 
 import Utils
@@ -20,25 +20,26 @@ globalconfigfile = do
 setGlobalName :: String -> IO ()
 setGlobalName n = do
   fpath <- globalconfigfile
-  writeFile fpath n
+  writeFile fpath (show n)
 
 -- throws if not found
 readGlobalName :: IO String
 readGlobalName = 
   do fpath <- globalconfigfile
-     exists <- doesDirectoryExist fpath
+     exists <- doesFileExist fpath
 
      if exists
        then do
          contents <- readFileStrict fpath
          case safeRead contents of
            Just n  -> return n
-           Nothing -> flipOut fpath
-       else flipOut fpath
+           Nothing -> flipOut userErrorType fpath
+       else flipOut doesNotExistErrorType fpath
 
-  where flipOut fpath = throwIO $ 
-          mkIOError doesNotExistErrorType 
+  where flipOut e fpath = throwIO $ 
+          mkIOError e 
              ("Could not read global configuration file - run nix config first.") 
              Nothing (Just fpath)
 
+getUserName = readGlobalName
 
